bc76a9ace0c6c2187044ead9c5a9c794a4591f9e
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2021-07-29 21:47:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var cursore = [1, 1];
+var contatoreLinee = 1;
+const numeroLinee = document.getElementById("numeroLinee");
+const testo = document.getElementById("testo");
+
+// guarda se è stato salvato il file
+if (localStorage.getItem('testo') !== null)
+    testo.innerHTML = localStorage.getItem('testo');
+
+function modificaNumeroLinee(numero) {
+    
+    if (numero > 0) 
+        for (let i = 0; i < numero; ++i) 
+            numeroLinee.innerHTML += `<span id="linea-${++contatoreLinee}" class="numeroLinea">${contatoreLinee}</span>`
+    else if (numero < 0)
+        for (let i = 0; i > numero; --i) 
+            document.getElementById(`linea-${contatoreLinee--}`).remove();
+
+}
+
+function aggiorna(event) {
+
+    let numeroDiLineeEffettive = testo.innerText.split(/\n\n|\n/).length;
+    console.log(numeroDiLineeEffettive + ' - ' + contatoreLinee);
+
+    modificaNumeroLinee(numeroDiLineeEffettive - contatoreLinee);
+
+    localStorage.setItem('testo', testo.innerHTML);
+
+}
+
+document.addEventListener("keydown", function(e) {
+
+    var key = undefined;
+    var possible = [ e.key, e.keyIdentifier, e.keyCode, e.which ];
+
+    while (key === undefined && possible.length > 0) key = possible.pop();
+
+    if (key && (key == '115' || key == '83' ) && (e.ctrlKey || e.metaKey) && !(e.altKey)) {
+
+        e.preventDefault();
+        
+        console.log(testo.innerText);
+
+        saveAs(new Blob([testo.innerText], {type: "text/plain;charset=utf-8"}), `${document.title}.txt`);
+
+        return false;
+    
+    }
+    
+    return true;
+}); 
